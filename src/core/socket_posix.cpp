f63aed4a32799419df6dc0670faf00bfff706ccc
howhwohwohwho <noonegibvesashitaboutyou@gmail.com>
2026-09-19 20:27:01 +0400

    Update ReXGlue iOS socket support

--- src/core/socket_posix.cpp
+++ src/core/socket_posix.cpp
@@ -1,8 +1,7 @@
 #include <rex/net/socket.h>
 #include <rex/platform.h>
 
-static_assert(REX_PLATFORM_LINUX || REX_PLATFORM_MAC, "This file is POSIX-only");
-
+static_assert(REX_PLATFORM_LINUX || REX_PLATFORM_MAC || REX_PLATFORM_IOS, "This file is POSIX-only");
 #include <sys/ioctl.h>
 #include <unistd.h>
 
