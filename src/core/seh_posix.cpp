c7fc9d123f97c2111d8db3746affb7e7ea0aba27
howhwohwohwho <noonegibvesashitaboutyou@gmail.com>
2026-09-19 20:51:23 +0400

    Add iOS support to POSIX SEH

--- src/core/seh_posix.cpp
+++ src/core/seh_posix.cpp
@@ -9,7 +9,7 @@
 #include <rex/platform.h>
 #include <rex/platform/seh.h>
 
-static_assert(REX_PLATFORM_LINUX || REX_PLATFORM_MAC, "This file is POSIX-only");
+static_assert(REX_PLATFORM_LINUX || REX_PLATFORM_MAC || REX_PLATFORM_IOS, "This file is POSIX-only");
 
 #include <signal.h>
 #include <cstdlib>  // std::abort()
